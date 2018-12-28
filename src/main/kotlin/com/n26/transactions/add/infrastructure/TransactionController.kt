a70aa1c7bb59be50ddfbcc2f6780f799c6b6ecc4
Alvaro Garcia <derecerca@gmail.com>
2018-12-28 15:58:44 +0100

    Green

--- src/main/kotlin/com/n26/transactions/add/infrastructure/TransactionController.kt
+++ src/main/kotlin/com/n26/transactions/add/infrastructure/TransactionController.kt
@@ -4,8 +4,9 @@ import arrow.core.Either
 import com.n26.transactions.TransactionService
 import com.n26.transactions.add.infrastructure.delivery.AddTransaction
 
-class TransactionController(service: TransactionService) {
+class TransactionController(private val service: TransactionService) {
     fun addTransaction(request: AddTransaction): Either<TransactionError, TransactionSuccess> {
+        service.addTransaction(request)
         return Either.right(TransactionSuccess())
     }
 }
