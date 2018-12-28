0ec6e5c7f1703475baf0876d873673c5b6aad758
Alvaro Garcia <derecerca@gmail.com>
2018-12-28 16:54:56 +0100

    Refactor: extract explaining methods

--- src/test/java/com/n26/transactions/add/infrastructure/test/AddTransactionUnitTest.java
+++ src/test/java/com/n26/transactions/add/infrastructure/test/AddTransactionUnitTest.java
@@ -5,6 +5,7 @@ import com.n26.transactions.TransactionService;
 import com.n26.transactions.add.infrastructure.TransactionController;
 import com.n26.transactions.add.infrastructure.TransactionSuccess;
 import com.n26.transactions.add.infrastructure.delivery.AddTransaction;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,28 +20,33 @@ import static org.mockito.Mockito.*;
 public class AddTransactionUnitTest {
 
 
+    public static final AddTransaction REQUEST = new AddTransaction(new BigDecimal("12.3343"), parseDate("2018-07-17T09:59:51.312Z"));
+
     @Test
     public void delegate_the_call_to_the_service() {
-        TransactionService service = mock(TransactionService.class);
-        when(service.addTransaction(any())).thenReturn(Either.Companion.right(new TransactionSuccess()));
-        final AddTransaction request = new AddTransaction(new BigDecimal("12.3343"), parseDate("2018-07-17T09:59:51.312Z"));
+        TransactionService service = serviceResponding();
         TransactionController controller = new TransactionController(service);
 
-        controller.addTransaction(request);
+        controller.addTransaction(REQUEST);
 
-        verify(service).addTransaction(request);
+        verify(service).addTransaction(REQUEST);
     }
 
     @Test
     public void a_valid_response_is_mapped() {
-        TransactionService service = mock(TransactionService.class);
-        when(service.addTransaction(any())).thenReturn(Either.Companion.right(new TransactionSuccess()));
+        TransactionService service = serviceResponding();
         TransactionController controller = new TransactionController(service);
-        final AddTransaction request = new AddTransaction(new BigDecimal("12.3343"), parseDate("2018-07-17T09:59:51.312Z"));
 
-        final ResponseEntity<?> responseEntity = controller.addTransaction(request);
+        final ResponseEntity<?> responseEntity = controller.addTransaction(REQUEST);
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isEqualTo(null);
+    }
+
+    @NotNull
+    private TransactionService serviceResponding() {
+        TransactionService service = mock(TransactionService.class);
+        when(service.addTransaction(any())).thenReturn(Either.Companion.right(new TransactionSuccess()));
+        return service;
     }
 }
